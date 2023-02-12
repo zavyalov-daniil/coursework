061839efe43dfcbf3c7b5c919e952107f0b17066
Daniil Zavyalov <dezavv1@gmail.com>
2023-02-12 20:33:45 +0600

    [+].gitignore

---
+++ src/test/java/com/zavyalov/kiselev/coursework/CourseworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zavyalov.kiselev.coursework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
