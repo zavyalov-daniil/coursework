29f0c0cfb558c6785460f5b06e3e5c4b1bbd3164
Kiselev Danil <dabikis2855@gmail.com>
2023-03-31 15:56:21 +0600

    [+] Queue interface

---
+++ src/main/java/com/zavyalov/kiselev/coursework/service/IQueue.java
@@ -0,0 +1,11 @@
+package com.zavyalov.kiselev.coursework.service;
+
+import com.zavyalov.kiselev.coursework.service.Commands.ICommand;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface IQueue {
+    void put(ICommand cmd);
+
+    void execNext();
+}
