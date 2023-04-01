a033172e2cc331f8d5e8c8a51b5e0628fe2c507a
Kiselev Danil <dabikis2855@gmail.com>
2023-04-01 22:44:50 +0600

    [+] empty class for command-based IPostService realisation

---
+++ src/main/java/com/zavyalov/kiselev/coursework/service/PostCommandService.java
@@ -0,0 +1,55 @@
+package com.zavyalov.kiselev.coursework.service;
+
+import com.zavyalov.kiselev.coursework.form.PostForm;
+import com.zavyalov.kiselev.coursework.view.PostView;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PostCommandService implements IPostService {
+
+
+    /* Должен отвечать за контроль полномочий у различных ролей, + менеджерить peer-to-peer разрешения */
+
+    private IQueue queue;
+
+    public PostCommandService(IQueue queue) {
+        this.queue = queue;
+    }
+
+    @Override
+    public List<PostView> getAllPosts() {
+        return null;
+    }
+
+    @Override
+    public PostView findPostById(Integer postId) {
+        return null;
+    }
+
+    @Override
+    public PostView save(PostForm postForm) {
+        return null;
+    }
+
+    @Override
+    public PostView changeTitle(Integer id, String title) {
+        return null;
+    }
+
+    @Override
+    public PostView changeText(Integer id, String text) {
+        return null;
+    }
+
+    @Override
+    public void delete(Integer postId) {
+
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
+}
