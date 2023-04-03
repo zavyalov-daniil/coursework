4ab5390095f0a9680dbe09cf6237ee3cd6d8525c
Daniil Zavyalov <dezavv1@gmail.com>
2023-04-03 21:22:31 +0600

    Entities and repositories for Neo and Postges

--- src/main/java/com/zavyalov/kiselev/coursework/entity/PostEntity.java
+++ src/main/java/com/zavyalov/kiselev/coursework/entity/PostEntity.java
@@ -1,11 +1,23 @@
 package com.zavyalov.kiselev.coursework.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.Date;
 
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class PostEntity {
-    Integer id;
+    @Id
+    Long id;
     String title;
     String text;
-    Date creationTime;
-    PostEntity parentPost;
+    //Date creationTime;
 }
