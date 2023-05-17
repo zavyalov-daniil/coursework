a92d32c6e6ee36c08583d871e0249722633a49ad
Kiselev Danil <dabikis2855@gmail.com>
2023-05-17 11:08:31 +0600

    repository for permissions

---
+++ src/main/java/com/zavyalov/kiselev/coursework/repository/PermissionRepository.java
@@ -0,0 +1,9 @@
+package com.zavyalov.kiselev.coursework.repository;
+
+import com.zavyalov.kiselev.coursework.entity.PermissionEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PermissionRepository extends JpaRepository<PermissionEntity, Integer> {
+}
