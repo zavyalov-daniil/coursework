f6743223982a37c525cfbe26127694eac7ab9736
Kiselev Danil <dabikis2855@gmail.com>
2023-05-24 20:36:25 +0600

    [+] bean for filling map with permission handler

--- src/main/java/com/zavyalov/kiselev/coursework/config/PermissionsConfig.java
+++ src/main/java/com/zavyalov/kiselev/coursework/config/PermissionsConfig.java
@@ -1,7 +1,14 @@
 package com.zavyalov.kiselev.coursework.config;
 
+import com.zavyalov.kiselev.coursework.entity.PermissionEntity;
+import com.zavyalov.kiselev.coursework.repository.PermissionRepository;
+import com.zavyalov.kiselev.coursework.service.permissions.DefaultPermissionHandler;
+import com.zavyalov.kiselev.coursework.service.permissions.IPermissionHandler;
+import org.springframework.boot.web.reactive.context.AnnotationConfigReactiveWebApplicationContext;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Scope;
 
 import java.util.ArrayList;
@@ -26,5 +33,27 @@ public class PermissionsConfig {
     public String role() {
         String currentRole = "testRole";
         return currentRole;
+    }
+
+    @Bean
+    @Scope(value = "singleton")
+    @Lazy
+    public Map<String, IPermissionHandler> permissionHandlerMap(PermissionRepository permissionRepository) {
+        Map<String, IPermissionHandler> map = new HashMap<>();
+        for (PermissionEntity entity : permissionRepository.findAll()) {
+            try {
+                ApplicationContext context = new AnnotationConfigReactiveWebApplicationContext();
+                IPermissionHandler handler = context.getBean(entity.getHandlerName() + "PermissionHandler", IPermissionHandler.class); //Прописать правила нейминга бинов
+                map.put(entity.getPermissionName(), handler);
+            } catch (Exception ex) {
+                map.put(entity.getPermissionName(), getDefaultHandler());
+            }
+        }
+
+        return map;
+    }
+
+    public IPermissionHandler getDefaultHandler() {
+        return new DefaultPermissionHandler();
     }
 }
