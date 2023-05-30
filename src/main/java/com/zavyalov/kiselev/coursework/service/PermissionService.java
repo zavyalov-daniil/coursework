32ec52acca02f10a5dc14056c0687d2f8f1032a9
Kiselev Danil <dabikis2855@gmail.com>
2023-05-30 18:08:35 +0600

    [+] find permission by name, method to get permission handler by name

--- src/main/java/com/zavyalov/kiselev/coursework/service/PermissionService.java
+++ src/main/java/com/zavyalov/kiselev/coursework/service/PermissionService.java
@@ -1,22 +1,30 @@
 package com.zavyalov.kiselev.coursework.service;
 
+import com.zavyalov.kiselev.coursework.config.PermissionHandlersConfig;
 import com.zavyalov.kiselev.coursework.entity.PermissionEntity;
 import com.zavyalov.kiselev.coursework.repository.PermissionRepository;
+import com.zavyalov.kiselev.coursework.service.permissions.DefaultPermissionHandler;
+import com.zavyalov.kiselev.coursework.service.permissions.IPermissionHandler;
 import com.zavyalov.kiselev.coursework.view.PermissionView;
 import lombok.AllArgsConstructor;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Service
 @AllArgsConstructor
 public class PermissionService {
-    final PermissionRepository repository;
+    final PermissionRepository permissionRepository;
 
     public List<PermissionView> getAllPermissions() {
-        List<PermissionEntity> entList = repository.findAll();
+        List<PermissionEntity> entList = permissionRepository.findAll();
         List<PermissionView> viewList = new ArrayList<>();
         for (PermissionEntity ent : entList) {
             viewList.add(new PermissionView(ent.getPermissionId(), ent.getPermissionName()));
@@ -25,11 +33,31 @@ public class PermissionService {
     }
 
     public Optional<PermissionView> getById(Long id) {
-        Optional<PermissionEntity> ent = repository.findById(id);
+        Optional<PermissionEntity> ent = permissionRepository.findById(id);
         if (ent.isEmpty()) {
             return Optional.empty();
         }
         Optional<PermissionView> view = Optional.of(new PermissionView(ent.get().getPermissionId(), ent.get().getPermissionName()));
         return view;
+    }
+
+    public PermissionEntity findPermissionByName(String permissionName) {
+        Optional<PermissionEntity> opt = permissionRepository.findPermissionEntitiesByPermissionName(permissionName);
+        return opt.orElse(null);
+    }
+
+    private IPermissionHandler getDefaultHandler() {
+        return new DefaultPermissionHandler();
+    }
+
+    public IPermissionHandler permissionHandler(String handlerName) {
+        IPermissionHandler ph = null;
+        ApplicationContext context = new AnnotationConfigApplicationContext(PermissionHandlersConfig.class);//new AnnotationConfigReactiveWebApplicationContext();
+        try {
+            ph = context.getBean(handlerName + "PermissionHandler", IPermissionHandler.class);
+        } catch (Exception ex) {
+            ph = getDefaultHandler();
+        }
+        return ph;
     }
 }
