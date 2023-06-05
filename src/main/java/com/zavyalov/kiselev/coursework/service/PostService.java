2b195c98683052fd40a25e170f242191c5d12915
Daniil Zavyalov <dezavv1@gmail.com>
2023-06-05 23:40:39 +0600

    First permission check

--- src/main/java/com/zavyalov/kiselev/coursework/service/PostService.java
+++ src/main/java/com/zavyalov/kiselev/coursework/service/PostService.java
@@ -1,11 +1,15 @@
 package com.zavyalov.kiselev.coursework.service;
 
 import com.zavyalov.kiselev.coursework.entity.PostNodeEntity;
+import com.zavyalov.kiselev.coursework.exception.PostNotFoundException;
 import com.zavyalov.kiselev.coursework.form.PostForm;
 import com.zavyalov.kiselev.coursework.repository.PostNeo4jRepository;
 import com.zavyalov.kiselev.coursework.service.lambda.ChangePostField;
+import com.zavyalov.kiselev.coursework.service.permissions.PermissionManager;
 import com.zavyalov.kiselev.coursework.view.PostView;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.dao.PermissionDeniedDataAccessException;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
 import java.util.Map;
 import java.util.Optional;
@@ -16,20 +20,25 @@ public class PostService {
     final PostConverter converter;
     Map<String, ChangePostField> changePostFieldMap;
 
+    final PermissionManager permissionManager;
+
     public PostService(PostNeo4jRepository neo4jRepository, PostConverter converter,
-                       @Qualifier("changePostFieldMap") Map<String, ChangePostField> changePostFieldMap) {
+                       @Qualifier("changePostFieldMap") Map<String, ChangePostField> changePostFieldMap,
+                       PermissionManager permissionManager) {
         this.neo4jRepository = neo4jRepository;
         this.converter = converter;
         this.changePostFieldMap = changePostFieldMap;
+        this.permissionManager = permissionManager;
     }
 
-    public PostForm getPostForm() {
-        return new PostForm();
-    }
-
-    public Optional<PostView> findPostById(Long postId) {
-        Optional<PostNodeEntity> res = neo4jRepository.findById(postId);
-        return res.map(entity -> converter.postNodeEntityToView(entity));
+    public Optional<PostView> findPostById(Long postId) throws Exception {
+        Boolean hasPermission = permissionManager.checkPermission("READ_ALL_NOTES");
+        if (hasPermission) {
+            Optional<PostNodeEntity> res = neo4jRepository.findById(postId);
+            return res.map(entity -> converter.postNodeEntityToView(entity));
+        } else {
+            throw new AccessDeniedException("User doesn't have the requested permission");
+        }
     }
 
     public PostView save(PostForm postForm) {
