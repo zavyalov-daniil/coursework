d586061616afe8ab0d1eebf0abae87199b3a6af2
Kiselev Danil <dabikis2855@gmail.com>
2023-06-05 11:09:45 +0600

    [+] now works with userEntity

--- src/main/java/com/zavyalov/kiselev/coursework/service/permissions/PermissionManager.java
+++ src/main/java/com/zavyalov/kiselev/coursework/service/permissions/PermissionManager.java
@@ -2,13 +2,18 @@ package com.zavyalov.kiselev.coursework.service.permissions;
 
 import com.zavyalov.kiselev.coursework.entity.PermissionEntity;
 import com.zavyalov.kiselev.coursework.entity.RoleEntity;
+import com.zavyalov.kiselev.coursework.entity.UserEntity;
+import com.zavyalov.kiselev.coursework.exception.RoleNotFoundException;
+import com.zavyalov.kiselev.coursework.exception.UserNotFoundException;
 import com.zavyalov.kiselev.coursework.repository.RoleRepository;
+import com.zavyalov.kiselev.coursework.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 
 @Service
@@ -20,13 +25,17 @@ public class PermissionManager implements IPermissionManager {
     PermissionService permissionService;
     //    RoleService roleService;
     RoleRepository roleRepository;
+    UserRepository userRepository;
+    String userName;
 
     /**
      * Checks first- and second- lvl permissions
      */
-    public Boolean checkPermission(String permissionName) throws Exception {
+    public Boolean checkPermission(String permissionName, String userName) throws Exception {
         //todo return microservice.
+        this.userName = userName;
         return checkMapRole(permissionName) || checkMapUser(permissionName);
+
     }
 
     private Map<String, IPermissionHandler> handlers;
@@ -38,13 +47,22 @@ public class PermissionManager implements IPermissionManager {
 
         boolean result = false;
 
-
+        //Взять юзера
+        Optional<UserEntity> userEntity = userRepository.findByLogin(userName);
+        if (userEntity.isEmpty()) {
+            throw new UserNotFoundException();
+        }
         //Взять роль
-        RoleEntity role = roleRepository.findById(1L).get();
+
+        Optional<RoleEntity> role = roleRepository.findById(userEntity.get().getRole().getRoleId());//roleRepository.findById(1L).get();
+        if (role.isEmpty()) {
+            throw new RoleNotFoundException();
+        }
         //(RoleEntity) permissionsContext.getBean("role");//Не подходит, нужна Entity, получать через IRole
 
         //Взять у роли список разрешений
-        Set<PermissionEntity> currentRolePermissions = role.getPermissionsSet();
+        Set<PermissionEntity> currentRolePermissions = role.get().getPermissionsSet();
+
 
         //Чекнуть в списке разрешений нужное
         PermissionEntity permission = permissionService.findPermissionByName(permissionName);
