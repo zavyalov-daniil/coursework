15c839c48c95d6352f3277c5ead9f72deabe90cd
Kiselev Danil <dabikis2855@gmail.com>
2023-06-06 14:54:04 +0600

    [+] works with permissionList

--- src/main/java/com/zavyalov/kiselev/coursework/service/permissions/RoleService.java
+++ src/main/java/com/zavyalov/kiselev/coursework/service/permissions/RoleService.java
@@ -1,13 +1,17 @@
 package com.zavyalov.kiselev.coursework.service.permissions;
 
+import com.zavyalov.kiselev.coursework.entity.PermissionEntity;
 import com.zavyalov.kiselev.coursework.entity.RoleEntity;
 import com.zavyalov.kiselev.coursework.repository.RoleRepository;
+import com.zavyalov.kiselev.coursework.view.PermissionView;
 import com.zavyalov.kiselev.coursework.view.RoleView;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Service
 @AllArgsConstructor
@@ -17,8 +21,13 @@ public class RoleService {
     public List<RoleView> getAllRoles() {
         List<RoleEntity> rolesEnt = roleRepository.findAll();
         List<RoleView> roleViews = new ArrayList<>(rolesEnt.size());
+
         for (RoleEntity ent : rolesEnt) {
-            roleViews.add(new RoleView(ent.getRoleId(), ent.getRoleName()));
+            Set<PermissionView> pv = new HashSet<>();
+            for (PermissionEntity pe : ent.getPermissionsSet()) {
+                pv.add(new PermissionView(pe.getPermissionId(), pe.getPermissionName()));
+            }
+            roleViews.add(new RoleView(ent.getRoleId(), ent.getRoleName(), pv));
         }
         return roleViews;
     }
