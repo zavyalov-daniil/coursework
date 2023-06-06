b8b2724f0c19ce32cd3b17d9dc5accbac6bf1cf9
Kiselev Danil <dabikis2855@gmail.com>
2023-06-06 14:51:23 +0600

    [+] works with permission set

--- src/main/java/com/zavyalov/kiselev/coursework/service/security/UserConverter.java
+++ src/main/java/com/zavyalov/kiselev/coursework/service/security/UserConverter.java
@@ -1,18 +1,29 @@
 package com.zavyalov.kiselev.coursework.service.security;
 
+import com.zavyalov.kiselev.coursework.entity.PermissionEntity;
 import com.zavyalov.kiselev.coursework.entity.UserEntity;
 import com.zavyalov.kiselev.coursework.form.RegisterForm;
+import com.zavyalov.kiselev.coursework.view.PermissionView;
 import com.zavyalov.kiselev.coursework.view.RoleView;
 import com.zavyalov.kiselev.coursework.view.UserView;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Component
 @AllArgsConstructor
 public class UserConverter {
     public UserView entityToView(UserEntity entity) {
         if (entity.getRole() != null) {
-            return new UserView(entity.getId(), entity.getLogin(), new RoleView(entity.getRole().getRoleId(), entity.getRole().getRoleName()));
+            Set<PermissionView> pv = new HashSet<>();
+            for (PermissionEntity pe : entity.getRole().getPermissionsSet()) {
+                pv.add(new PermissionView(pe.getPermissionId(), pe.getPermissionName()));
+            }
+            return new UserView(entity.getId(), entity.getLogin(), new RoleView(entity.getRole().getRoleId(), entity.getRole().getRoleName(), pv));
         } else {
             return new UserView(entity.getId(), entity.getLogin(), null);
         }
